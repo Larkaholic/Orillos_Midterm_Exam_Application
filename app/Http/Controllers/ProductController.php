a134bdd88b06f1eedf3688250b8f8109d3aeaf28
Larkaholic <orillosdominique@gmail.com>
2025-02-20 22:06:26 +0800

    ..

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = [
-            ['name' => 'Computer', 'price' => 35000],
-            ['name' => 'Printer', 'price' => 25000],
-            ['name' => 'Smartphone', 'price' => 20000]
-        ];
-
-        return view('products', compact('products'));
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
 }
